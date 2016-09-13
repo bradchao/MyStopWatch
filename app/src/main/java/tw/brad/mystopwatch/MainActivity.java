476a98e7c53d999ddaec00f9d5966f55313266c3
Brad Chao <brad@brad.tw>
2016-09-13 11:26:45 +0800

    v2

--- app/src/main/java/tw/brad/mystopwatch/MainActivity.java
+++ app/src/main/java/tw/brad/mystopwatch/MainActivity.java
@@ -1,13 +1,33 @@
 package tw.brad.mystopwatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLeft, btnRight;
+    private boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLeft = (Button)findViewById(R.id.btnLeft);
+        btnRight = (Button)findViewById(R.id.btnRight);
+
     }
+
+    // Reset / Lap
+    public void doLeft(View v){
+
+    }
+    // Start / Stop
+    public void doRight(View v){
+        isRunning = !isRunning;
+        btnRight.setText(isRunning?"Stop":"Start");
+        btnLeft.setText(isRunning?"Lap":"Reset");
+    }
+
 }
